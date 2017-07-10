3f0cb2c6be7dcd2abeaf883cd28bec939a0fe188
Yassine Ammar <ammar.yassine@yahoo.fr>
2017-07-10 16:24:43 +0200

    Add lib

---
+++ src/main/java/Views/BasicView.java
@@ -0,0 +1,34 @@
+package Views;
+
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class BasicView {
+
+  public class HelloWorld {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+      return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+      this.firstName = firstName;
+    }
+
+    public String getLastName() {
+      return lastName;
+    }
+
+    public void setLastName(String lastName) {
+      this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+      return "Hello" + " " + firstName + " " + lastName + "!";
+    }
+  }
+}
